2ebbbfb6c06a86d70cc336807b1741112a4e86ce
seungmin <seungmin4625@gmail.com>
2023-10-26 00:26:47 +0900

    update

---
+++ express-js/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/currenttime", function (req, res) {
+  res.send("<h1>" + new Date().toISOString() + "</h1>");
+});
+
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.listen(3000);
