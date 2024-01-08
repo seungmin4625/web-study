476aec9e3eb9f780609e60e66a36ff589fdf5224
seungmin <seungmin4625@gmail.com>
2024-01-08 23:52:34 +0900

    database end

--- database/routes/blog.js
+++ database/routes/blog.js
@@ -8,8 +8,13 @@ router.get('/', function (req, res) {
   res.redirect('/posts');
 });
 
-router.get('/posts', function (req, res) {
-  res.render('posts-list');
+router.get('/posts', async function (req, res) {
+  const query = `
+    SELECT posts.*, authors.name AS author_name FROM posts
+    INNER JOIN authors ON posts.author_id = authors.id
+  `;
+  const [posts] = await db.query(query);
+  res.render('posts-list', { posts: posts });
 });
 
 router.get('/new-post', async function (req, res) {
@@ -17,16 +22,85 @@ router.get('/new-post', async function (req, res) {
   res.render('create-post', { authors: authors });
 });
 
-router.post('/posts', function (req, res) {
+router.post('/posts', async function (req, res) {
   const data = [
     req.body.title,
     req.body.summary,
     req.body.content,
     req.body.author,
   ];
-  db.query('INSERT INTO posts (title, summary, body, author_id) VALUES (?)', [
-    data
+  await db.query(
+    'INSERT INTO posts (title, summary, body, author_id) VALUES (?)',
+    [data]
+  );
+  res.redirect('/posts');
+});
+
+router.get('/posts/:id', async function (req, res) {
+  const query = `
+    SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM posts
+    INNER JOIN authors ON posts.author_id = authors.id
+    WHERE posts.id = ?
+  `;
+
+  const [posts] = await db.query(query, [req.params.id]);
+
+  if (!posts || posts.length === 0) {
+    return res.status(404).render('404');
+  }
+
+  const postData = {
+    ...posts[0],
+    date: posts[0].date.toISOString(),
+    humanReadableDate: posts[0].date.toLocaleDateString('ko-KR', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    }),
+  };
+
+  res.render('post-detail', { post: postData });
+});
+
+router.get('/posts/:id/edit', async function (req, res) {
+  const query = `
+    SELECT * FROM posts WHERE id = ?
+  `;
+
+  const [posts] = await db.query(query, [req.params.id]);
+
+  if (!posts || posts.length === 0) {
+    return res.status(404).render('404');
+  }
+
+  res.render('update-post', { post: posts[0] });
+});
+
+router.post('/posts/:id/edit', async function (req, res) {
+  const query = `
+    UPDATE posts SET title = ?, summary = ?, body = ?
+    WHERE id = ?
+  `;
+
+  await db.query(query, [
+    req.body.title,
+    req.body.summary,
+    req.body.content,
+    req.params.id,
   ]);
+
+  res.redirect('/posts');
+});
+
+router.post('/posts/:id/delete', async function (req, res) {
+  const query = `
+    DELETE FROM posts WHERE id = ?
+  `;
+
+  await db.query(query, [req.params.id]);
+
+  res.redirect('/posts');
 });
 
 module.exports = router;
