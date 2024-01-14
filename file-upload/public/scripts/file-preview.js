a959690141f966bc77471f8a088b566ab5d86a43
seungmin <seungmin4625@gmail.com>
2024-01-14 23:12:59 +0900

    update

---
+++ file-upload/public/scripts/file-preview.js
@@ -0,0 +1,8 @@
+const filePickerElement = document.getElementById('image');
+const imagePreviewElement = document.getElementById('image-preview');
+
+function showPreview() {
+  filePickerElement.files;
+}
+
+filePickerElement.addEventListener('change', showPreview);
